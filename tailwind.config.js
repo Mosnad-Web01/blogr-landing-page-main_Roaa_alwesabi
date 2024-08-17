0d4002e1044fa80b00f21b70ba6287c1dbc77726
Roaa2002-web <roaa.abdulfattah.alwesabi@gmail.com>
2024-08-17 16:01:46 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// tailwind.config.js
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'custom-slate': 'hsl(237, 23%, 32%)',
+      },
+    },
+  },
+}
